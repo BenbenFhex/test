5b9c7f08f55b6008101d991fcc02b49bde29c477
BenbenFhex <cai556579@gmail.com>
2025-06-23 17:24:49 -0500

    Update server2.py

--- server2.py
+++ server2.py
@@ -1,1 +1,11 @@
 
+def main():
+    while True:
+        server2 = input("")  # No prompt text
+        with open("server2_data.txt", "w") as f:
+            f.write(server2)
+
+if __name__ == "__main__":
+    main()
+
+
